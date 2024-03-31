b3796c4b24983e1eb4d451493ff839bc83d31acd
Srikanth Reddy Gunukula <srikanthgunukula21@gmail.com>
2024-03-31 19:32:13 -0400

    completed fetchChat route

--- air-chat/backend/Models/chatModel.js
+++ air-chat/backend/Models/chatModel.js
@@ -7,7 +7,7 @@ const chatModel = mongoose.Schema(
     users: [
       {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "user",
+        ref: "User",
       },
     ],
     latestMessage: {
