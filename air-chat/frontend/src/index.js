f3ce8eba2a1764baab9713cad31b35739ac114ce
Srikanth Reddy Gunukula <srikanthgunukula21@gmail.com>
2024-04-01 17:44:27 -0400

    started with context API

--- air-chat/frontend/src/index.js
+++ air-chat/frontend/src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { ChakraProvider } from '@chakra-ui/react';
-import {BrowserRouter} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+import ChatProvider from "./Context/ChatProvider";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
+  <ChatProvider>
     <BrowserRouter>
-    <ChakraProvider>
-    <App />
-    </ChakraProvider>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  </ChatProvider>,
+  document.getElementById("root")
 );
