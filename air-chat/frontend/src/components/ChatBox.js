dfb89c0590018f1b1411fb3ebc7d289ec296e26a
Srikanth Reddy Gunukula <srikanthgunukula21@gmail.com>
2024-04-11 19:21:10 -0400

    updated ChatBox and SingleChat

--- air-chat/frontend/src/components/ChatBox.js
+++ air-chat/frontend/src/components/ChatBox.js
@@ -7,13 +7,13 @@ const ChatBox = ({ fetchAgain, setFetchAgain }) => {
   const { selectedChat } = ChatState();
   return (
     <Box
-      d={{ base: selectedChat ? "flex" : "none", md: "flex" }}
+      display={{ base: selectedChat ? "flex" : "none", md: "flex" }}
       alignItems={"center"}
       flexDir={"column"}
-      p={3}
+      padding={3}
       bg={"white"}
-      w={{ base: "100%", md: "68%" }}
-      borderRadius={"large"}
+      width={{ base: "100%", md: "68%" }}
+      borderRadius={"1%"}
       borderWidth={"1px"}
     >
       <SingleChat fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
--- air-chat/frontend/src/components/SingleChat.js
+++ air-chat/frontend/src/components/SingleChat.js
@@ -1,13 +1,32 @@
 import React from "react";
 import { ChatState } from "../Context/ChatProvider";
 import { Box, Text } from "@chakra-ui/react";
+import { ArrowBackIcon } from "@chakra-ui/icons";
+import { IconButton } from "@chakra-ui/button";
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
   const { user, selectedChat, setSelectedChat } = ChatState();
   return (
     <>
       {selectedChat ? (
-        <></>
+        <>
+          <Text
+            fontSize={{ base: "28px", md: "30px" }}
+            pb={3}
+            px={2}
+            w={"100%"}
+            fontFamily={"Work sans"}
+            display={"flex"}
+            justifyContent={{ base: "space-between" }}
+            alignItems={"center"}
+          >
+            <IconButton
+              display={{ base: "flex", md: "none" }}
+              icon={<ArrowBackIcon />}
+              onClick={() => setSelectedChat("")}
+            />
+          </Text>
+        </>
       ) : (
         <Box
           display={"flex"}
@@ -15,11 +34,7 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
           justifyContent={"center"}
           h={"100%"}
         >
-          <Text
-            fontSize={"xxx-large"}
-            paddingBottom={3}
-            fontFamily={"Work sans"}
-          >
+          <Text fontSize={"3xl"} paddingBottom={3} fontFamily={"Work sans"}>
             Click on a user to Start Chatting
           </Text>
         </Box>
