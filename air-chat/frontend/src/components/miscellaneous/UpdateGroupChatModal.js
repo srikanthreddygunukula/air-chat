fbfc896cd508077a98ea3e9c8c2c024d3b38810d
Srikanth Reddy Gunukula <srikanthgunukula21@gmail.com>
2024-04-15 21:11:54 -0400

    improved the GroupChat for adding users and removing

--- air-chat/frontend/src/components/miscellaneous/UpdateGroupChatModal.js
+++ air-chat/frontend/src/components/miscellaneous/UpdateGroupChatModal.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDisclosure } from "@chakra-ui/hooks";
 import { IconButton, Button } from "@chakra-ui/button";
 import { InfoIcon } from "@chakra-ui/icons";
@@ -10,21 +10,77 @@ import {
   ModalFooter,
   ModalBody,
   ModalCloseButton,
+  FormControl,
+  Input,
 } from "@chakra-ui/react";
+import { ChatState } from "../../Context/ChatProvider";
+import { useToast, Box } from "@chakra-ui/react";
+import UserBadgeItem from "../UserAvatar/UserBadgeItem";
 
 const UpdateGroupChatModal = ({ fetchAgain, setFetchAgain }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const { selectedChat, setSelectedChat, user } = ChatState();
+  const [groupChatName, setGroupChatName] = useState();
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [renameLoading, setRenameLoading] = useState(false);
+  const toast = useToast();
+  const handleRemove = () => {};
+  const handleRename = () => {};
+  const handleSearch = () => {};
 
   return (
     <>
       <IconButton d={{ base: "flex" }} icon={<InfoIcon />} onClick={onOpen} />
 
-      <Modal isOpen={isOpen} onClose={onClose}>
+      <Modal isOpen={isOpen} onClose={onClose} isCentered>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
+          <ModalHeader
+            fontSize="30px"
+            fontFamily={"Work sans"}
+            display={"flex"}
+            justifyContent={"center"}
+          >
+            {selectedChat.chatName}
+          </ModalHeader>
           <ModalCloseButton />
-          <ModalBody></ModalBody>
+          <ModalBody>
+            <Box w={"100%"} display={"flex"} flexWrap={"wrap"} pb={3}>
+              {selectedChat.users.map((u) => (
+                <UserBadgeItem
+                  key={user._id}
+                  user={u}
+                  handleFunction={() => handleRemove(u)}
+                />
+              ))}
+            </Box>
+            <FormControl display={"flex"}>
+              <Input
+                placeholder="Chat Name"
+                mb={3}
+                vlaue={groupChatName}
+                onChange={(e) => setGroupChatName(e.target.value)}
+              />
+              <Button
+                variant={"solid"}
+                colorScheme="teal"
+                ml={1}
+                isLoading={renameLoading}
+                onClick={handleRename}
+              >
+                Update
+              </Button>
+            </FormControl>
+            <FormControl>
+              <Input
+                placeholder="Add User to group"
+                mb={1}
+                onChange={(e) => handleSearch(e.target.value)}
+              />
+            </FormControl>
+          </ModalBody>
 
           <ModalFooter>
             <Button colorScheme="blue" mr={3} onClick={onClose}>
