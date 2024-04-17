af762430792f317911343b61c0fb47cd97b0a6b4
Srikanth Reddy Gunukula <srikanthgunukula21@gmail.com>
2024-04-16 22:10:32 -0400

    completed backend logic for sending and fetching messages

--- air-chat/backend/routes/messageRoutes.js
+++ air-chat/backend/routes/messageRoutes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const { protect } = require("../middlewares/authMiddleware");
-const { sendMessage } = require("../controllers/messageControllers");
+const { sendMessage, allMessages } = require("../controllers/messageControllers");
 
 const router = express.Router();
 
 router.route("/").post(protect, sendMessage);
-// router.route("/:chatId").get(protect, allMessages);
+router.route("/:chatId").get(protect, allMessages);
 
 module.exports = router;
