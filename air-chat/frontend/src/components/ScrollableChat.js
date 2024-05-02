31354d70921d050df912e053c6419a624d448177
Srikanth Reddy Gunukula <srikanthgunukula21@gmail.com>
2024-05-01 21:59:44 -0400

    modified ScrollableChat

--- air-chat/frontend/src/components/ScrollableChat.js
+++ air-chat/frontend/src/components/ScrollableChat.js
@@ -1,7 +1,35 @@
 import React from "react";
+import ScrollableFeed from "react-scrollable-feed";
+import { isLastMessage, isSamesender } from "../config/ChatLogics";
+import { Tooltip, Avatar } from "@chakra-ui/react";
 
 const ScrollableChat = ({ messages }) => {
-  return <div></div>;
+  return (
+    <ScrollableFeed>
+      {messages &&
+        messages.map((m, i) => (
+          <div style={{ display: "flex" }} key={m._id}>
+            {isSamesender(messages, m, i, user._id) ||
+              (isLastMessage(messages, m, i, user._id) && (
+                <Tooltip
+                  label={m.sender.name}
+                  placement="bottom-start"
+                  hasArrow
+                >
+                  <Avatar
+                    mt={"7px"}
+                    mr={1}
+                    size={"sm"}
+                    cursor={"pointer"}
+                    name={m.sender.name}
+                    src={m.sender.pic}
+                  />
+                </Tooltip>
+              ))}
+          </div>
+        ))}
+    </ScrollableFeed>
+  );
 };
 
 export default ScrollableChat;
