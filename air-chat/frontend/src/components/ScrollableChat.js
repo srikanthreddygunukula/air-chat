7c1d13c62469a2dedeb633d9c28ccd686df7dfc5
Srikanth Reddy Gunukula <srikanthgunukula21@gmail.com>
2024-05-03 22:25:12 -0400

    Not able to see messages in singlechat

--- air-chat/frontend/src/components/ScrollableChat.js
+++ air-chat/frontend/src/components/ScrollableChat.js
@@ -2,30 +2,28 @@ import React from "react";
 import ScrollableFeed from "react-scrollable-feed";
 import { isLastMessage, isSamesender } from "../config/ChatLogics";
 import { Tooltip, Avatar } from "@chakra-ui/react";
+import { ChatState } from "../Context/ChatProvider";
 
 const ScrollableChat = ({ messages }) => {
+  const { user } = ChatState();
   return (
     <ScrollableFeed>
       {messages &&
         messages.map((m, i) => (
           <div style={{ display: "flex" }} key={m._id}>
             {(isSamesender(messages, m, i, user._id) ||
-              isLastMessage(messages, m, i, user._id) && (
-                <Tooltip
-                  label={m.sender.name}
-                  placement="bottom-start"
-                  hasArrow
-                >
-                  <Avatar
-                    mt={"7px"}
-                    mr={1}
-                    size={"sm"}
-                    cursor={"pointer"}
-                    name={m.sender.name}
-                    src={m.sender.pic}
-                  />
-                </Tooltip>
-              )}
+              isLastMessage(messages, m, i, user._id)) && (
+              <Tooltip label={m.sender.name} placement="bottom-start" hasArrow>
+                <Avatar
+                  mt={"7px"}
+                  mr={1}
+                  size={"sm"}
+                  cursor={"pointer"}
+                  name={m.sender.name}
+                  src={m.sender.pic}
+                />
+              </Tooltip>
+            )}
             <span
               style={{
                 backgroundColor: `${
