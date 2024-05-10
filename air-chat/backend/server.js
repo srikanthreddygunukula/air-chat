089f010ccf22a10b7e3b42a1c5b52a90172e0243
Srikanth Reddy Gunukula <srikanthgunukula21@gmail.com>
2024-05-10 04:05:50 -0400

    made changes for moving app to production

--- air-chat/backend/server.js
+++ air-chat/backend/server.js
@@ -7,6 +7,7 @@ const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+const path = require("path");
 
 const app = express();
 dotenv.config();
@@ -14,15 +15,27 @@ connectDB();
 
 app.use(express.json()); // to accept JSON Data
 
-app.get("/", (req, res) => {
-  res.send("API is Running Sucessfully");
-});
-
 app.use("/api/user", userRoutes);
 
 app.use("/api/chat", chatRoutes);
 
 app.use("/api/message", messageRoutes);
+
+// ------------------------Deployment------------------
+
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is Running Sucessfully");
+  });
+}
+// ------------------------Deployment------------------
 
 app.use(notFound);
 app.use(errorHandler);
